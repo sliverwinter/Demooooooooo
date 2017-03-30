65c2d6b1be69f981593f8c1d06adfc5267b49e22
Teamnavi <Teamnavi@DESKTOP-UUFR2CD>
2017-03-30 17:56:09 +0800

    Change-Id: I4525a17e0a9b158b9ba8749e53545ef83f00a4d1

---
+++ src/singleton/InsertSort.java
@@ -0,0 +1,34 @@
+package singleton;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InsertSort {
+
+	public static List<Integer> insertSort(final List<Integer> numbers) {
+		final List<Integer> sortedList = new LinkedList<>();
+
+		//?????????????????????????
+		originalList: for (Integer number : numbers) {
+			for (int i = 0; i < sortedList.size(); i++) {
+				if (number < sortedList.get(i)) {
+					sortedList.add(i, number);
+					continue originalList;
+				}
+			}
+			sortedList.add(sortedList.size(), number);
+
+		}
+		return sortedList;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> Integer = new ArrayList<Integer>();
+		Integer.add(1);
+		Integer.add(4);
+		Integer.add(2);
+		System.out.println(insertSort(Integer));
+	}
+}
