49fa2d3ecd8fb0f60748df7fec9eb8423a6417bc
Teamnavi <Teamnavi@DESKTOP-UUFR2CD>
2017-03-30 18:22:52 +0800

    Change-Id: I3470781cd18eef70de53bbda5ea23bfbee5f7a7d

--- src/com/zkz/demo/reflect/Test1.java
+++ src/com/zkz/demo/reflect/Test1.java
@@ -1,5 +1,19 @@
 package com.zkz.demo.reflect;
 
 public class Test1 {
+	public static void main(String[] args) {
+
+		Class<?> c = null;
+		try {
+			c = Class.forName("com.zkz.demo.reflect.Student");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	Student s = null;
+	
 
 }
