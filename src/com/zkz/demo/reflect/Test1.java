3745a91138dc61794737131a038620a198d1c660
Teamnavi <Teamnavi@DESKTOP-UUFR2CD>
2017-03-30 18:39:07 +0800

    Change-Id: Ib0771d3afa2f876d554f21faf5b0dd7e7bf26418

--- src/com/zkz/demo/reflect/Test1.java
+++ src/com/zkz/demo/reflect/Test1.java
@@ -1,5 +1,8 @@
 package com.zkz.demo.reflect;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Test1 {
 	public static void main(String[] args) {
 		
@@ -7,14 +10,20 @@ public class Test1 {
 		Class<?> c = null;
 		try {
 			c = Class.forName("com.zkz.demo.reflect.Student");
+			System.out.println(c.getName());
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		Student s = null;
+		Constructor<?>[] cons=c.getConstructors();
+		try {
+			s=(Student) cons[1].newInstance("xiaofeng",111);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(s);
 
 	}
-
-	Student s = null;
-	
 
 }
