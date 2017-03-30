0a057e298be67bdf17f5a7d855f99efda856d7b3
Teamnavi <Teamnavi@DESKTOP-UUFR2CD>
2017-03-30 18:25:02 +0800

    Change-Id: I9a5ddf0481002d727e55de6e44df41038fca89b0

--- src/com/zkz/demo/reflect/Test1.java
+++ src/com/zkz/demo/reflect/Test1.java
@@ -2,7 +2,8 @@ package com.zkz.demo.reflect;
 
 public class Test1 {
 	public static void main(String[] args) {
-
+		
+		//
 		Class<?> c = null;
 		try {
 			c = Class.forName("com.zkz.demo.reflect.Student");
